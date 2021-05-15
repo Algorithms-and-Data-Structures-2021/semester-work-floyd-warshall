7ae7ea3bb3cc908fc3cdb32f19251559493177c7
RizvanGaripov <rizvangaripov@gmail.com>
2021-05-15 16:43:11 +0300

    final realization floyd-warshall algorithm

--- main.cpp
+++ main.cpp
@@ -6,12 +6,14 @@ using namespace std;
 
 int main() {
   vector<vector<int>> w = {{0, 5, 9, 100}, {100, 0, 2, 8}, {100, 100, 0, 7}, {4, 100, 100, 0}};
-  itis::floyd(w);
+  vector<vector<int>> d = itis::floyd(w);
   for (int i = 0; i < 4; ++i) {
     for (int j = 0; j < 4; ++j) {
-      cout << w[i][j] << " ";
+      cout << d[i][j] << " ";
     }
     cout << endl;
   }
+
+  cout << itis::getShortestPath(0, 3);
   return 0;
 }
--- src/floyd-warshall_algorithm.cpp
+++ src/floyd-warshall_algorithm.cpp
@@ -4,18 +4,42 @@
 
 namespace itis {
 
-  void floyd(vector<vector<int>> &w) {
-    vector<vector<int>> next = w;
-    for (int i = 0; i < w.size(); ++i) {
-      for (int u = 0; u < w.size(); ++u) {
-        for (int v = 0; v < w.size(); ++v) {
-          if (w[u][i] + w[i][v] < w[u][v]) {
-            w[u][v] = w[u][i] + w[i][v];
+  vector<vector<int>> floyd(vector<vector<int>> &w) {
+    d = w;
+    //
+    next.resize(w.size());
+    for (int u = 0; u < w.size(); ++u) {
+      for (int v = 0; v < w.size(); ++v) {
+        next[u].push_back(v);
+      }
+    }
+
+    for (int i = 0; i < d.size(); ++i) {
+      for (int u = 0; u < d.size(); ++u) {
+        for (int v = 0; v < d.size(); ++v) {
+          if ((d[u][i] != -1 && d[i][v] != -1) && (d[u][i] + d[i][v] < d[u][v])) {
+            d[u][v] = d[u][i] + d[i][v];
             next[u][v] = next[u][i];
           }
         }
       }
     }
+    return d;
+  }
+
+  string getShortestPath(int u, int v) {
+    if (d[u][v] == -1) {
+      return "No path found";
+    }
+
+    string result = "";
+    int c = u;
+    while (c != v) {
+      result = result + to_string(c) + " ";
+      c = next[c][v];
+    }
+    result += to_string(v);
+    return result;
   }
 
 }  // namespace itis
